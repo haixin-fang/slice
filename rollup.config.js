8ad8e3192f24f747145345e94ec216ae75cbdbd7
haixin <haixin.fang@ly.com>
2022-12-04 23:15:43 +0800

    feat: 打包结果优化,初始化调试功能,事件继承

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,57 @@
-import resolve from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
-import commonjs from '@rollup/plugin-commonjs';
-
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+import commonjs from "@rollup/plugin-commonjs";
+import es3 from "rollup-plugin-es3";
+import { uglify } from "rollup-plugin-uglify";
+const ts = typescript({
+  module: "es2015",
+  target: "es3",
+  lib: ["es2015", "dom"],
+  exclude: "node_modules/**",
+  sourceMap: true,
+});
 export default [
   {
-    input: './src/index.ts',
+    input: "./src/index.ts",
     output: {
-      dir: 'dist',
-      format: 'cjs',
-      entryFileNames: '[name].cjs.js',
+      dir: "dist",
+      format: "cjs",
+      name: "slice",
+      entryFileNames: "slice.cjs.js",
+      sourcemap: true,
     },
-    plugins: [resolve(), commonjs(), typescript()],
-  }, {
-    input: './src/index.ts',
+    plugins: [es3({ sourcemap: true }), resolve(), commonjs(), ts],
+  },
+  {
+    input: "./src/index.ts",
     output: {
-      dir: 'dist',
-      format: 'esm',
-      entryFileNames: '[name].esm.js',
+      dir: "dist",
+      format: "esm",
+      name: "slice",
+      entryFileNames: "slice.esm.js",sourcemap: true,
     },
-    plugins: [resolve(), commonjs(), typescript()],
-  }
+    plugins: [es3({ sourcemap: true }), resolve(), commonjs(), ts],
+  },
+  {
+    input: "src/index.ts",
+    plugins: [es3({ sourcemap: true }), resolve(), commonjs(), ts],
+    output: {
+      dir: "dist",
+      format: "umd",
+      name: "slice",
+      entryFileNames: "slice.js",
+      sourcemap: true,
+    },
+  },
+  {
+    input: "src/index.ts",
+    plugins: [es3({ sourcemap: true }), uglify(), resolve(), commonjs(), ts],
+    output: {
+      dir: "dist",
+      format: "umd",
+      name: "slice",
+      entryFileNames: "slice.min.js",
+      sourcemap: true,
+    },
+  },
 ];
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
-const func = () => {
-    // TODO: coding...
-};
-
-export { func };
+import { EventEmitter } from "events";
+class Slice extends EventEmitter {
+  public options: any = {};
+  constructor(options: any) {
+    super();
+    this.options = options;
+  }
+}
+export default Slice;
